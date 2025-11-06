5d2eb4972ccea9c2d3dac2a1ccacc0cbbaad8734
Lucian Crasovan <LUCA@xxxlgroup.com>
2025-11-06 18:02:22 +0000

    try again

--- modules/examples/consumer/src/test/java/com/github/austek/example/pulsar/avro/PactPulsarConsumerTest.java
+++ modules/examples/consumer/src/test/java/com/github/austek/example/pulsar/avro/PactPulsarConsumerTest.java
@@ -39,18 +39,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(PactConsumerTestExt.class)
 @PactTestFor(
-        pactVersion = PactSpecVersion.V4,
-        providerType = ProviderType.ASYNCH,
-        providerName = "avro-plugin-provider")
+    pactVersion = PactSpecVersion.V4,
+    providerType = ProviderType.ASYNCH,
+    providerName = "avro-plugin-provider")
 class PactPulsarConsumerTest {
   private final String schemasPath;
   private final OrderService orderService = new OrderService();
 
   PactPulsarConsumerTest() throws URISyntaxException {
     schemasPath =
-            Paths.get(Objects.requireNonNull(getClass().getResource("/avro/orders.avsc")).toURI())
-                    .toFile()
-                    .getAbsolutePath();
+        Paths.get(Objects.requireNonNull(getClass().getResource("/avro/orders.avsc")).toURI())
+            .toFile()
+            .getAbsolutePath();
   }
 
   public static <T> List<T> arrayByteToAvroRecord(Class<T> c, byte[] bytes) throws IOException {
@@ -85,34 +85,34 @@ class PactPulsarConsumerTest {
   V4Pact configureRecordWithDependantRecord(PactBuilder builder) {
     // tag::configuration[]
     var messageBody =
-            Map.of(
-                    "message.contents",
-                    Map.ofEntries(
-                            Map.entry("pact:avro", schemasPath),
-                            Map.entry("pact:record-name", "Order"),
-                            Map.entry("pact:content-type", "avro/binary"),
-                            Map.entry("id", "notEmpty('100')"),
-                            Map.entry("names", "notEmpty('name-1')"),
-                            Map.entry("enabled", "matching(boolean, true)"),
-                            Map.entry("height", "matching(decimal, 15.8)"),
-                            Map.entry("width", "matching(decimal, 1.8)"),
-                            Map.entry("status", "matching(equalTo, 'CREATED')"),
-                            Map.entry(
-                                    "address",
-                                    Map.of(
-                                            "no", "matching(integer, 121)",
-                                            "street", "matching(equalTo, 'street name')")),
-                            Map.entry(
-                                    "items",
-                                    List.of(
-                                            Map.of("name", "notEmpty('Item-1')", "id", "notEmpty('1')"),
-                                            Map.of("name", "notEmpty('Item-2')", "id", "notEmpty('2')"))),
-                            Map.entry("userId", "notEmpty('20bef962-8cbd-4b8c-8337-97ae385ac45d')")));
+        Map.of(
+            "message.contents",
+            Map.ofEntries(
+                Map.entry("pact:avro", schemasPath),
+                Map.entry("pact:record-name", "Order"),
+                Map.entry("pact:content-type", "avro/binary"),
+                Map.entry("id", "notEmpty('100')"),
+                Map.entry("names", "notEmpty('name-1')"),
+                Map.entry("enabled", "matching(boolean, true)"),
+                Map.entry("height", "matching(decimal, 15.8)"),
+                Map.entry("width", "matching(decimal, 1.8)"),
+                Map.entry("status", "matching(equalTo, 'CREATED')"),
+                Map.entry(
+                    "address",
+                    Map.of(
+                        "no", "matching(integer, 121)",
+                        "street", "matching(equalTo, 'street name')")),
+                Map.entry(
+                    "items",
+                    List.of(
+                        Map.of("name", "notEmpty('Item-1')", "id", "notEmpty('1')"),
+                        Map.of("name", "notEmpty('Item-2')", "id", "notEmpty('2')"))),
+                Map.entry("userId", "notEmpty('20bef962-8cbd-4b8c-8337-97ae385ac45d')")));
     return builder
-            .usingPlugin("avro", "0.1.0")
-            .expectsToReceive("Order Created", "core/interaction/message")
-            .with(messageBody)
-            .toPact();
+        .usingPlugin("avro", "0.1.0")
+        .expectsToReceive("Order Created", "core/interaction/message")
+        .with(messageBody)
+        .toPact();
     // end::configuration[]
   }
   // end::consumer_test[]
@@ -121,19 +121,19 @@ class PactPulsarConsumerTest {
   @Test
   @PactTestFor(pactMethod = "configureRecordWithDependantRecord")
   void consumerRecordWithDependantRecord(V4Interaction.AsynchronousMessage message)
-          throws IOException {
+      throws IOException {
     MessageContents messageContents = message.getContents();
     List<Order> orders =
-            arrayByteToAvroRecord(Order.class, messageContents.getContents().getValue());
+        arrayByteToAvroRecord(Order.class, messageContents.getContents().getValue());
     Order order = assertFirstOrder(orders);
 
     assertThat(messageContents.getContents().getContentType())
-            .hasToString("avro/binary; record=Order");
+        .hasToString("avro/binary; record=Order");
     assertThat(messageContents.getContents().getContentTypeHint())
-            .isEqualTo(ContentTypeHint.BINARY);
+        .isEqualTo(ContentTypeHint.BINARY);
 
     Map<String, MatchingRuleCategory> ruleCategoryMap =
-            ((MatchingRulesImpl) messageContents.getMatchingRules()).getRules();
+        ((MatchingRulesImpl) messageContents.getMatchingRules()).getRules();
     assertThat(ruleCategoryMap).hasSize(1);
     Map<String, MatchingRuleGroup> rules = ruleCategoryMap.get("body").getMatchingRules();
     List<MatchingRule> idRules = rules.get("$.id").getRules();
@@ -144,7 +144,7 @@ class PactPulsarConsumerTest {
     assertThat(name0Rules.get(0)).extracting("name").isEqualTo("not-empty");
 
     assertThat(order.getUserId())
-            .isEqualTo(UUID.fromString("20bef962-8cbd-4b8c-8337-97ae385ac45d"));
+        .isEqualTo(UUID.fromString("20bef962-8cbd-4b8c-8337-97ae385ac45d"));
 
     assertDoesNotThrow(() -> orderService.process(order));
   }
